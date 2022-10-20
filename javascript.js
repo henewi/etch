0be1e277164233099316e6d1f9180ce2601d91d1
henewi <hpdunn2002@gmail.com>
2022-10-20 22:40:02 +0100

    drawing function complete!

--- javascript.js
+++ javascript.js
@@ -9,3 +9,42 @@ function makeRows(rows, cols) {
   };
 };
 makeRows(16, 16);
+
+// restart //
+const clearBtn = document.querySelector('button');
+clearBtn.addEventListener('click', () => {
+  grid(0)
+  container.innerHTML = ''
+})
+
+// drawing //
+const grid = document.getElementById("container");
+
+let colorTrigger = false;
+
+container.addEventListener('mousedown', () => {      
+  colorTrigger = true;  
+});   
+
+container.addEventListener('mouseup', () => {      
+  colorTrigger = false;  
+});
+
+function draw(color){                                           //draw function with current color
+  let coloredRowElements = document.querySelectorAll('.grid-item'); //Create list of grid elements
+  
+  for (let coloredRowElement of coloredRowElements){
+      
+      coloredRowElement.addEventListener('mousedown', () => {
+          coloredRowElement.style.backgroundColor = color;             
+      })  
+      
+      coloredRowElement.addEventListener('mousemove', () => {
+          if (colorTrigger)
+          coloredRowElement.style.backgroundColor = color;                 
+      })
+  }            
+}
+
+color = document.getElementById("color").value; 
+draw(color)
